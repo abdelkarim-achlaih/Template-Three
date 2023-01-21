4599e67e07999db5d9abff2641cf327f0638b2a3
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-01-21 22:53:36 +0100

    Updating Events Section: hover border + creating a countdown timer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Start Skills Section */
 let skillsSection = document.querySelector("section.skills");
 let skills = document.querySelectorAll(".skill-progress");
 
@@ -11,3 +12,28 @@ window.onscroll = function () {
 		}
 	});
 };
+/* End Skills Section */
+/* Start Events Section */
+let countUnits = document.querySelectorAll(".count .num");
+let deadLine = new Date("21 January 2024 23:59:59");
+
+let eventCounter = setInterval(() => {
+	let now = new Date();
+	let difference = deadLine - now;
+	let countDown = [];
+	let days = difference / (1000 * 60 * 60 * 24);
+	let hours = (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+	let minutes = (difference % (1000 * 60 * 60)) / (1000 * 60);
+	let seconds = (difference % (1000 * 60)) / 1000;
+	countDown.unshift(days, hours, minutes, seconds);
+	for (let i = 0; i < countUnits.length; i++) {
+		countUnits[i].innerHTML = countDown[i].toFixed(0);
+	}
+	if (difference <= 0) {
+		clearInterval(eventCounter);
+		for (let i = 0; i < countUnits.length; i++) {
+			countUnits[i].innerHTML = 0;
+		}
+	}
+}, 1000);
+/* End Events Section */
